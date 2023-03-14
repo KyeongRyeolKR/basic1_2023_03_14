93debddbe2d9ce30c3089cda80cf6a9918002cd3
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-14 11:22:57 +0900

    C:/Program Files/Git/home/main2, /home/main3

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -10,6 +10,18 @@ public class HomeController {
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain() {
-        return "환영합니다.";
+        return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
     }
 }
