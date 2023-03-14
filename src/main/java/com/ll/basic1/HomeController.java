486bc74ec2a13e30557b5607c24bf7977da86148
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-14 13:08:09 +0900

    home/plus

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -34,5 +35,11 @@ public class HomeController {
     @ResponseBody
     public int showIncrease() {
         return index++;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
     }
 }
