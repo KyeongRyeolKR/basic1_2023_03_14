e2a859a70b0fc511d595f610b24c8989262eb521
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-14 13:42:57 +0900

    home/addPerson, home/people

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
     private int index;
+    private int lastId;
+    private List<Person> people;
 
     public HomeController() {
         index = 0;
+        lastId = 1;
+        people = new ArrayList<>();
     }
 
     @GetMapping("/home/main")
@@ -41,5 +49,18 @@ public class HomeController {
     @ResponseBody
     public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
         return a + b;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String showAddPerson(@RequestParam String name, @RequestParam int age) {
+        people.add(new Person(lastId, name, age));
+        return (lastId++) + "번 사람이 추가되었습니다.";
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
     }
 }
---
+++ src/main/java/com/ll/basic1/Person.java
@@ -0,0 +1,14 @@
+package com.ll.basic1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+}
