849ad8019b577e5392bda7d0c67f87b21504acbc
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-16 15:56:19 +0900

    me.html 타임리프 적용

--- src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
@@ -1,12 +1,12 @@
 package com.ll.basic1.boundedContext.member.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
-@Data
+@ToString
 public class Member {
     private static long lastId;
     private long id;
